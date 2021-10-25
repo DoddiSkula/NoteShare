f450f1a3173c3e080f8d77066e79c3e7a2b5233e
Doddi <doddiskula@gmail.com>
2021-10-25 21:43:23 +0000

    view grunnur

--- src/main/java/is/hi/noteshare/controllers/CourseController.java
+++ src/main/java/is/hi/noteshare/controllers/CourseController.java
@@ -1,4 +1,31 @@
 package is.hi.noteshare.controllers;
 
+import is.hi.noteshare.persistence.entities.Course;
+import is.hi.noteshare.services.CourseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class CourseController {
+
+    CourseService courseService;
+
+    /*
+    @Autowired
+    public CourseController(CourseService courseService){
+        this.courseService = courseService;
+    }
+     */
+
+    @RequestMapping(value = "/course/{id}", method = RequestMethod.GET)
+    public String courseGET(@PathVariable("id") long id, Model model){
+        // Course course = courseService.findByID(id);
+        return "course";
+    }
+
+
 }
--- src/main/java/is/hi/noteshare/controllers/HomeController.java
+++ src/main/java/is/hi/noteshare/controllers/HomeController.java
@@ -1,6 +1,7 @@
 package is.hi.noteshare.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -8,8 +9,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HomeController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String home(){
+    public String home(Model model){
         return "index";
     }
+
 
 }
