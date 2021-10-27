4bdc4042ea0b67c5fd1938645b6a614edd41339b
Doddi <doddiskula@gmail.com>
2021-10-27 19:42:15 +0000

    properties

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,7 +5,7 @@ insert into schools(name, level) values ('University of Iceland', 'University');
 insert into schools(name, level) values ('Reykjavik University', 'University');
 
 -- users
-insert into users(school_id, username, password, email, admin) values (1, 'admin', 'admin', 'icehot1@althingi.is', true);
+insert into users(school_id, username, password, email, admin) values (1, 'admin', 'admin', 'admin@admin.com', true);
 
 -- courses
 insert into courses(school_id, short_name, name) values (1, 'HBV501G', 'Software Project 1');
