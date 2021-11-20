6d4079b09e5b0cf8be7dd0009db580c9e5f83ca8
Rögnvaldur Bjarnason <56052147+RoggoDude@users.noreply.github.com>
2021-11-20 17:25:43 +0000

    Upload fikt

--- src/main/java/is/hi/noteshare/controllers/FileController.java
+++ src/main/java/is/hi/noteshare/controllers/FileController.java
@@ -1,7 +1,36 @@
 package is.hi.noteshare.controllers;
 
+import is.hi.noteshare.persistence.entities.Course;
+import is.hi.noteshare.persistence.entities.User;
+import is.hi.noteshare.services.CourseService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class FileController {
+    private final CourseService courseService;
+
+    public FileController(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
+    @RequestMapping(value = "/course/{id}/upload", method = RequestMethod.GET)
+    public String courseGET(@PathVariable("id") long id, Model model, HttpSession session) {
+        Course course = courseService.findById(id);
+
+        // get logged in user
+        User userSession = (User) session.getAttribute("loggedInUser");
+        if(userSession != null) {
+            model.addAttribute("loggedInUser", userSession);
+        }
+
+        model.addAttribute("course", course);
+
+        return "upload";
+    }
 }
